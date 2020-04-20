2fe712db725039cb6867681ef003f4fae0433bc2
Stipan97 <stjepan.posavec@gmail.com>
2020-04-20 11:36:20 +0200

    Start init

---
+++ 2-Blackjack-LV1/app/src/main/java/com/example/a2_blackjack_lv1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a2_blackjack_lv1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
